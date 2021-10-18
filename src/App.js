3cd0d696233536d6e886ef5e50ce963aabfd18db
miob1781 <67827043+miob1781@users.noreply.github.com>
2021-10-18 22:10:11 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import styled from 'styled-components'
 
+const AppBody = styled.body`
+  width: 800px;
+  background-color: white;
+  font-family: Calibri;
+`
+
+const Header = styled.header`
+  width: 100%;
+  font-size: 1.5em;
+`
+
+const Main = styled.main`
+  width: 100%;
+`
+
+const Footer = styled.footer`
+  width: 100%;
+  font-size: 0.8em;
+`
+
+function App() {
+  return (
+    <AppBody>
+      <Header>
+        <h1>Mensch ärgere dich nicht!</h1>
+      </Header>
+      <Main>
+
+      </Main>
+      <Footer>
+
+      </Footer>
+    </AppBody>
+  )
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +58,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
